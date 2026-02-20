5f3d1d2262120f1eeb0971b616882a37664df678
Saqib Ismail <saqibismail@Saqibs-MacBook-Air.local>
2026-02-20 14:13:27 -0600

    firebase initialized and repo setup

---
+++ app/functions/index.js
@@ -0,0 +1,11 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+app.get("/health", (req, res) => res.json({ ok: true }));
+
+exports.api = functions.https.onRequest(app);
